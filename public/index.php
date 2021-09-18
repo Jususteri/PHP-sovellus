dd0021e8f01f95c9114cd8829e0d07a2cbd46947
Jere Luomanmäki <jere.luomanmaki@edu.sasky.fi>
2021-09-19 01:49:16 +0300

    koodin kommentointia

--- public/index.php
+++ public/index.php
@@ -1,20 +1,33 @@
 <?php
 
-// Suoritetaan projektin alustusskripti.
+// Suoritetaan projektin alustusskripti nimeltä init.php.
+// init.php ajaa aluksi config kansiosta config.php:n ja näin ottaa
+// koko sovellusta koskevat asetukset käyttöön. 
+// Sen jälkeen init.php kutsuu vendor kansiosta autoload.php:tä ja ajaa sen
 require_once '../src/init.php';
 
+  // Tällä koodirivillä poistetaan polku kutsuttuun skriptiin.
+  // Jos sivua kutsutaan osoitteella /~jluomanm/lanify/public, 
+  // niin rivin jälkeen jää jäljelle /public. 
 
-  // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
-  // Siistimisen jälkeen osoite /~koodaaja/lanify/tapahtuma?id=1 on 
-  // lyhentynyt muotoon /tapahtuma.
+  // config.php:ssä on määritelty, että baseUrl = /~jluomanm/lanify/public
+  // $_SERVER['REQUEST_URI'] = URI joka annettiin päästäkseen tälle sivulle
   $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
+ 
+  // Tässä poistetaan vielä selaimelta tulleesta pyynnöstä urlin lopussa
+  // olevat parametrit, jotka erotetaan ?-merkillä osoitteesta. 
+  // Jos edellisen vaiheen jälkeen pyyntö on muotoa /tapahtuma?id=1, 
+  // niin tämän vaiheen jälkeen on jäljellä /tapahtuma.
   $request = strtok($request, '?');
 
-   // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
+  
+   // config.php:ssä on määritelty, että TEMPLATE_DIR = src/view. 
+   // Seuraavaksi luodaan uusi plates-olio ja kytketään se sivupohjiin, eli 
+   // Näkymä(View).
    $templates = new League\Plates\Engine(TEMPLATE_DIR);
 
-  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
-  // käsittelijä.
+  // Selvitetään ehtolauseilla mitä sivua on kutsuttu 
+  // ja suoritetaan sivua vastaava käsittelijä.
   if ($request === '/' || $request === '/tapahtumat') {
     echo $templates->render('tapahtumat');
   } else if ($request === '/tapahtuma') {
